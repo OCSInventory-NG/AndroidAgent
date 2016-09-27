d8144db9befbbff61594417efce8c7e4dc36b977
Anael Mobilia <contrib@anael.eu>
2016-09-27 08:16:06 +0200

    Ajout licence dans le project Cf #4

--- app/src/main/java/org/ocsinventoryng/android/agent/util/SystemUiHiderBase.java
+++ app/src/main/java/org/ocsinventoryng/android/agent/util/SystemUiHiderBase.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2013-2016, OCSInventory-NG/AndroidAgent contributors
+ *
+ * This file is part of OCSInventory-NG/AndroidAgent.
+ *
+ * OCSInventory-NG/AndroidAgent is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OCSInventory-NG/AndroidAgent is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OCSInventory-NG/AndroidAgent. If not, see <http://www.gnu.org/licenses/>
+ */
 package org.ocsinventoryng.android.agent.util;
 
 import android.app.Activity;
